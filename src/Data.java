044e5afe83137942be07a589777a687d79ef87d1
Wander29 <ludo.venturi@gmail.com>
2019-11-23 12:01:44 +0100

    progress

--- src/Data.java
+++ src/Data.java
@@ -1,10 +1,27 @@
 public abstract class Data {
-	
+/* 	OVERVIEW 		Data rappresenta un dato sottoforma di un insieme di 3 attributi e alcune operazioni
+ * 					È una struttura astratta immutable, di dimensione finita e fissa
+ * 
+ * TYPICAL ELEMENT 	{ dataName, content, category }
+ * 				tc
+ * 					dataName 	!= null
+ * 				&&	content 	!= null
+ * 				&&	cateogry 	!= null
+ */
 	private String dataName;
 	private String content;
 	private String category;
 	
-	public Data(String name, String cont, String cat) {
+	public Data(String name, String cont, String cat) throws NullPointerException {
+		/* @REQUIRES 		name != null
+		 * 				&&	cont != null
+		 * 				&& 	cat != null
+		 * @EFFECTS 	istanzia this
+		 * @THROWS 		if name == null || cont == null || cat == null
+		 * 					throws NullPointerException
+		 */
+		if(name == null || cont == null || cat == null) throw new NullPointerException();
+		
 		dataName = name;
 		content = cont;
 		category = cat;
@@ -24,11 +41,18 @@ public abstract class Data {
 		return this.content;
 	}
 	
-	public boolean equals(Data b) {
+	// restituisce true sse lo stato locale dei 2 oggetti è lo stesso, deep equality
+	public boolean equals(Data b) throws NullPointerException {
+		/* @REQUIRES 	b != null
+		 * @EFFECTS 	restituisce true se this e b denotano lo stesso oggetto relativamente ai dati contenuti
+		 * @THROWS 		if b == null
+		 * 					throws NullPointerException
+		 */
+		if (b == null) throw new NullPointerException();
+		
 		if ( 	this.dataName.equals(b.getDataName()) 
 			&&  this.category.equals(b.getCategory())
-			&& 	this.content.equals(b.getContent())
-			)
+			&& 	this.content.equals(b.getContent()) 	)
 			return true;
 		else
 			return false;
